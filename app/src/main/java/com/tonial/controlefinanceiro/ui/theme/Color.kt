70fc82d41f16840b458005b92fc546c9ee0947d4
Matheus <matheustonial97@gmail.com>
2026-01-01 13:06:29 -0300

    refactor: Update app theme and add icon assets
    This commit revamps the application's color scheme and introduces new drawable assets for the app icon.
    
    Key changes:
    - **Theme Overhaul**: Replaces the default purple-based color palette with a new green-centric theme.
      - Updates `DarkColorScheme` and `LightColorScheme` in `Theme.kt` to use new green, green-grey, and secondary green colors.
      - Defines the new green color values (`Green1`, `Green2`, `Green3`, `Green4`, `GreenGrey1`, `GreenGrey2`) in `Color.kt`.
    - **New Drawables**: Adds new vector drawable assets for the application's icon.
      - `money.xml`: A detailed vector graphic representing a hand drawing a dollar sign.
      - `money_bkgnd.xml`: A simple solid green background for the icon.
    - **Gradle Configuration**: Updates the `.idea/gradle.xml` file by adding `GradleMigrationSettings`.

--- app/src/main/java/com/tonial/controlefinanceiro/ui/theme/Color.kt
+++ app/src/main/java/com/tonial/controlefinanceiro/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Green1 = Color(0xFF56E571)
+val GreenGrey1 = Color(0xFFAEE6B9)
+val Green2 = Color(0xFFB1E65E)
+
+val Green3 = Color(0xFF07E670)
+val GreenGrey2 = Color(0xFF8DEA9A)
+val Green4 = Color(0xFF8DEA9A)
--- app/src/main/java/com/tonial/controlefinanceiro/ui/theme/Theme.kt
+++ app/src/main/java/com/tonial/controlefinanceiro/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green1,
+    secondary = GreenGrey1,
+    tertiary = Green2
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Green3,
+    secondary = GreenGrey2,
+    tertiary = Green4
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
